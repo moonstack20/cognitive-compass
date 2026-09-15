1e988c299677f30a92a17fb604e715b3a2291d93
moonstack20 <sheebshak@gmail.com>
2026-09-15 23:59:50 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+base:'/cogitive-compass/',
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
